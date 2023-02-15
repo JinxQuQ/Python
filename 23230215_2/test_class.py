8498b86b7de4bd1da7604d0cd9e8b2eb403b0e7d
lh <821669288@qq.com>
2023-02-15 23:49:49 +0800

    1

---
+++ 23230215_2/test_class.py
@@ -0,0 +1,16 @@
+# test_class.py
+
+#  content of  test_class.py
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert 'h' in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, 'check')
+
+    def test_three(self):
+        a = "hello"
+        b = "hello world"
+        assert a in b
